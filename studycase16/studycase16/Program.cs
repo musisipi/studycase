daf94f6bb22936e7d61d6e2eebd5a7b69f04ad42
M. Nur Fakhri <110540078+musisipi@users.noreply.github.com>
2023-10-27 18:36:30 +0700

    Add files via upload

---
+++ studycase16/studycase16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace studycase16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukan Umur Anda:");
+            int umur = Convert.ToInt32(Console.ReadLine());
+
+            {
+                if (umur >= 18)
+                    Console.WriteLine("Silahakan anda boleh menonton film ini. Selamat menonton!");
+                else
+                    Console.WriteLine("Anda belum cukup umur untuk menonton film ini. Silahkan untuk menonton film yang sesuai dengan umur anda");
+            }
+
+
+        }
+    }
+}
---
+++ studycase17/studycase17/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace studycase17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {                                                   
+            Console.Write("Masukan bilangan: ");
+            int bilangan = Convert.ToInt32(Console.ReadLine());
+
+            if (bilangan % 2 == 0)
+                Console.WriteLine("Bilangan adalah genap");
+
+
+            else
+                Console.WriteLine("Bilangan adalah ganjil");
+
+
+        }
+    }
+}
